3091c463c056b0bf40ea09db8d686b07eafce027
basubagoji123 <basubagoji123@gmail.com>
2018-10-30 23:20:04 +0530

    my commit from home

---
+++ src/main/java/com/user/Controller_User.java
@@ -0,0 +1,15 @@
+package com.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Controller_User {
+	
+	@RequestMapping("/test")
+	public String test(){
+		return "User test successfully!!";
+	}
+
+}
